c37a5fd47cc227c72990657a087eeeff222edc12
Jean-Pierre Gygax <gygax@practicomp.ch>
2016-04-07 17:05:55 +0200

    Now using a std::pair<> to combine data pointer and size into one type

--- include/gpc/fonts/store.hpp
+++ include/gpc/fonts/store.hpp
@@ -2,6 +2,7 @@
 
 #include <sstream>
 #include <map>
+#include <utility>
 
 #include <cereal/archives/binary.hpp>
 
@@ -11,26 +12,41 @@
 namespace gpc {
     
     namespace fonts {
-        
+
+        // TODO: replace with GSL span<> ?
+
+        using Data_store = std::pair<const uint8_t *, std::size_t>;
+
+        /* struct Data_store {
+            const uint8_t *data;
+            std::size_t    size;
+        }; */
+
         // TODO?: put this into its own header file "load.hpp" ?
 
-        inline auto load(const uint8_t *data, size_t size)
+        inline auto load(const Data_store &store)
         {
             rasterized_font rastfont;
             
-            std::stringstream is(std::string{ reinterpret_cast<const char *>(data), size });
+            std::stringstream is(std::string{ reinterpret_cast<const char *>(store.first), store.second });
             cereal::BinaryInputArchive archive(is);
             archive(rastfont);
             
             return rastfont;
         }
 
-        inline auto get(const uint8_t *data, size_t size) -> rasterized_font *
+        inline auto get(const Data_store &store) -> rasterized_font *
         {
             static std::map<const uint8_t *, std::unique_ptr<rasterized_font>> map;
 
-            return map.emplace(data, std::make_unique<rasterized_font>(load(data, size))).first->second.get();
+            return map.emplace(store.first, std::make_unique<rasterized_font>(load(store))).first->second.get();
         }
+
+        // FOR COMPATIBILITY
+
+        inline auto load(const uint8_t *data, size_t size) { return load({data, size}); }
+
+        inline auto get(const uint8_t *data, size_t size) { return get({data, size}); }
 
     } // ns fonts
     
