9c16aa301c8b003c05778f4ffa0bb900b7aae9d3
Jean-Pierre Gygax <gygax@practicomp.ch>
2016-06-04 17:25:22 +0200

    Made sizes signed; adding Conan support

---
+++ conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile, CMake
+
+class libGPCFontsConan(ConanFile):
+  name = "libGPCFonts"
+  version = "0.1.1"
+  #settings = "os", "compiler", "build_type", "arch"
+  #generators = "cmake"
+  exports = "include/*"
+
+  #def build(self):
+  #  cmake = CMake(self.settings)
+  #  self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
+  #  self.run("cmake --build . %s" % cmake.build_config)
+
+  def package(self):
+    self.copy("include/*", dst=".") # TODO: better way ?
+
+  #def package_info(self):
